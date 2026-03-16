e79dbdbffe86aed02b599263e25cc0055e9a2587
RanjanParida9817 <ranjanparida9817@gmail.com>
2026-03-16 14:46:12 +0530

    feat: add GET /books route. Closes #2

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 
 console.log("Hello");
 
+const books = [
+    {id:1,title:"1984"},
+    {id:2,title:"2020"}
+];
+
+app.get('/books',(req,res)=>{
+    res.status(200).json({
+        books:books
+    });
+});
+
 app.listen(3000,()=>{
     console.log("Server running at Port 3000");
 })
